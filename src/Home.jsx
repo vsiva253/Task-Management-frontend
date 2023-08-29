9a3596ac6fb39e0b2ea3d24c4e85ca0560a48d1e
vsiva253 <79691236+vsiva253@users.noreply.github.com>
2023-08-29 14:46:08 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -66,43 +66,49 @@ function Home() {
 
   return (
     <div className={`post-container ${completedContainerClass}`}>
-      {posts.map(post => (
-        <div key={post._id} className={`post-item ${post.isCompleted ? 'completed' : ''}`}>
-          <h2 className="post-title">{post.title}</h2>
-          <p className="post-description">{post.description}</p>
-          {!post.isCompleted ? (
-            <button className="completed-btn" onClick={() => markAsCompleted(post._id)}>
-              Mark as Completed
-            </button>
-          ) : (
-            <button className="completed-btn" disabled>
-              Completed
-            </button>
-          )}
-          <button onClick={() => deletePost(post._id)}>Delete</button>
-          {!post.isCompleted && (
-            <button onClick={() => handleEdit(post._id)}>Edit</button>
-          )}
-
-          {/* Display edit fields directly below the post */}
-          {editedPost && editedPost._id === post._id && (
-            <div className="edit-fields">
-              <input
-                type="text"
-                value={editedPost.title}
-                onChange={e => setEditedPost({ ...editedPost, title: e.target.value })}
-              />
-              <textarea
-                className="edit-description"
-                value={editedPost.description}
-                onChange={e => setEditedPost({ ...editedPost, description: e.target.value })}
-              />
-              <button onClick={handleUpdate}>Update</button>
-              <button onClick={() => setEditedPost(null)}>Cancel</button>
-            </div>
-          )}
+      {posts.length === 0 ? (
+        <div className="no-posts">
+          <p>Create Tasks</p>
         </div>
-      ))}
+      ) : (
+        posts.map(post => (
+          <div key={post._id} className={`post-item ${post.isCompleted ? 'completed' : ''}`}>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-description">{post.description}</p>
+            {!post.isCompleted ? (
+              <button className="completed-btn" onClick={() => markAsCompleted(post._id)}>
+                Mark as Completed
+              </button>
+            ) : (
+              <button className="completed-btn" disabled>
+                Completed
+              </button>
+            )}
+            <button onClick={() => deletePost(post._id)}>Delete</button>
+            {!post.isCompleted && (
+              <button onClick={() => handleEdit(post._id)}>Edit</button>
+            )}
+
+            {/* Display edit fields directly below the post */}
+            {editedPost && editedPost._id === post._id && (
+              <div className="edit-fields">
+                <input
+                  type="text"
+                  value={editedPost.title}
+                  onChange={e => setEditedPost({ ...editedPost, title: e.target.value })}
+                />
+                <textarea
+                  className="edit-description"
+                  value={editedPost.description}
+                  onChange={e => setEditedPost({ ...editedPost, description: e.target.value })}
+                />
+                <button onClick={handleUpdate}>Update</button>
+                <button onClick={() => setEditedPost(null)}>Cancel</button>
+              </div>
+            )}
+          </div>
+        ))
+      )}
     </div>
   );
 }
